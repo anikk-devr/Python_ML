cb769c8ad3e905d07b6ee02bbfdf7a77ddbdf594
anikk-devr <alaminanik2507@gmail.com>
2026-05-13 20:02:42 +0600

    Update OOP_practice.py

--- OOP_practice.py
+++ OOP_practice.py
@@ -1,1 +1,37 @@
+class Student: 
+    def __init__(self, name, dept, cgpa):
+        self.name = name
+        self.dept = dept
+        self.cgpa = cgpa
+
+    def show_info(self):
+        print("Name: ", self.name)
+        print("Dept: ", self.dept)
+        print("CGPA: ", self.cgpa)
+
+    def is_good_student(self):
+        if self.cgpa > 3.50:
+            print("Good Student")
+        else: 
+            print("Bad Studnet")
+
+
+s1 = Student("Anik", "CSE", 3.56)
+s1.show_info()
+s1.is_good_student()
+
+s2 = Student("Rahat", "EEE", 3.26)
+s2.show_info()
+s2.is_good_student()
+
+s3 = Student("Mahadi", "CSE", 3.16)
+s3.show_info()
+s3.is_good_student()
+
+
+
+
+
+
+
 
